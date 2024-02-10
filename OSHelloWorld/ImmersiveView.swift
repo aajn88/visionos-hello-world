def3a04d6c36e77bfc5d9e3637855c8e62dfa000
Toño <aajn88@gmail.com>
2024-02-10 18:34:04 +0000

    Init commit

--- OSHelloWorld/ImmersiveView.swift
+++ OSHelloWorld/ImmersiveView.swift
@@ -1,6 +1,6 @@
 //
 //  ImmersiveView.swift
-//  OSHelloWorld
+//  Hello World
 //
 //  Created by Antonio Jimenez on 10/02/2024.
 //
@@ -12,11 +12,50 @@ import RealityKitContent
 struct ImmersiveView: View {
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
+            let floor = ModelEntity(mesh: .generatePlane(width: 50, depth: 50), materials: [OcclusionMaterial()])
+            floor.generateCollisionShapes(recursive: false)
+            floor.components[PhysicsBodyComponent.self] = .init(
+                massProperties: .default,
+                material: .generate(staticFriction: 0.8, dynamicFriction: 0.5, restitution: 0.1),
+                mode: .static
+            )
+            
+            content.add(floor)
+            
+            if let appleModel = try? await Entity(named: "Apple_Reduced"), let apple = appleModel.children.first?.children.first {
+                let scale: Float = 3
+                apple.scale = [scale, scale, scale]
+                apple.position.y = 0.5
+                apple.position.z = -1
+                
+                apple.generateCollisionShapes(recursive: false)
+                apple.components.set(InputTargetComponent())
+                
+                apple.components[PhysicsBodyComponent.self] = .init(PhysicsBodyComponent(
+                    massProperties: .default,
+                    material: .generate(staticFriction: 0.8, dynamicFriction: 0.5, restitution: 0.1),
+                    mode: .dynamic))
+                
+                apple.components[PhysicsMotionComponent.self] = .init()
+                
+                content.add(apple)
             }
         }
+        .gesture(dragGesture)
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .targetedToAnyEntity()
+            .onChanged { value in
+                debugPrint("onChanged")
+                value.entity.position = value.convert(value.location3D, from: .local, to: value.entity.parent!)
+                value.entity.components[PhysicsBodyComponent.self]?.mode = .kinematic
+            }
+            .onEnded { value in
+                debugPrint("onEnded")
+                value.entity.components[PhysicsBodyComponent.self]?.mode = .dynamic
+            }
     }
 }
 
